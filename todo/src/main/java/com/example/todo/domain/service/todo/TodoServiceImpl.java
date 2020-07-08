07be0b0e42d3555a5aabd6dbeb6cbf21829f2b5d
T.K <2b9sa2emil@gmail.com>
2020-07-08 18:03:22 +0900

    例外処理の実装

--- todo/src/main/java/com/example/todo/domain/service/todo/TodoServiceImpl.java
+++ todo/src/main/java/com/example/todo/domain/service/todo/TodoServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.terasoluna.gfw.common.exception.BusinessException;
 import org.terasoluna.gfw.common.exception.ResourceNotFoundException;
-import org.terasoluna.gfw.common.message.ResultMessage;
 import org.terasoluna.gfw.common.message.ResultMessages;
 
 import com.example.todo.domain.model.Todo;
@@ -36,9 +35,7 @@ public class TodoServiceImpl implements TodoService {
 		Todo todo = todoRepository.findById(todoId).orElse(null);
 		if (todo == null) {
 			ResultMessages messages = ResultMessages.error();
-			messages.add(
-					ResultMessage.fromText(
-							"[E404] The requested Todo is not found. (id=" + todoId + ")"));
+			messages.add("E404", todoId);
 			// 対象データが存在しない場合は、ResourceNotFoundException
 			throw new ResourceNotFoundException(messages);
 		}
@@ -57,9 +54,7 @@ public class TodoServiceImpl implements TodoService {
 		long unfinishedCount = todoRepository.countByFinished(false);
 		if (unfinishedCount >= MAX_UNFINISHED_COUNT) {
 			ResultMessages messages = ResultMessages.error();
-			messages.add(
-					ResultMessage.fromText(
-							"[E001] The count of un-finished Todo must not be over " + MAX_UNFINISHED_COUNT + "."));
+			messages.add("E001", MAX_UNFINISHED_COUNT);
 			// 業務エラーの場合は、BusinessException
 			throw new BusinessException(messages);
 		}
@@ -80,9 +75,7 @@ public class TodoServiceImpl implements TodoService {
 		Todo todo = findOne(todoId);
 		if (todo.isFinished()) {
 			ResultMessages messages = ResultMessages.error();
-			messages.add(
-					ResultMessage.fromText(
-							"[E002] The requested Todo is already finished. (id=" + todoId + ")"));
+			messages.add("E002", todoId);
 			throw new BusinessException(messages);
 		}
 		todo.setFinished(true);
